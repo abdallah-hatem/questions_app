e7735d9ca035a81cd382e86368d4dc375c08ba6e
abdallah-hatem <ahkortam@gmail.com>
2024-09-24 19:03:58 +0300

    fix language

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,44 +2,52 @@ import {
   DarkTheme,
   DefaultTheme,
   ThemeProvider,
-} from "@react-navigation/native";
-import { useFonts } from "expo-font";
-import { Stack } from "expo-router";
-import * as SplashScreen from "expo-splash-screen";
-import { useEffect } from "react";
-import "react-native-reanimated";
-import { Colors } from "@/constants/Colors";
-import { useColorScheme } from "@/hooks/useColorScheme";
-import { Provider } from "react-redux";
-import { persistor, store } from "@/store";
-import { PersistGate } from "redux-persist/integration/react";
+} from "@react-navigation/native"
+import { useFonts } from "expo-font"
+import { Stack } from "expo-router"
+import * as SplashScreen from "expo-splash-screen"
+import { useEffect } from "react"
+import "react-native-reanimated"
+import { Colors } from "@/constants/Colors"
+import { useColorScheme } from "@/hooks/useColorScheme"
+import { Provider } from "react-redux"
+import { dispatch, persistor, store } from "@/store"
+import { PersistGate } from "redux-persist/integration/react"
 import {
   MD3LightTheme as PaperDefaultTheme,
   PaperProvider,
-} from "react-native-paper";
+} from "react-native-paper"
 
-import "@/i18";
-import { useTranslation } from "react-i18next";
+import "@/i18"
+import { useTranslation } from "react-i18next"
+import AsyncStorage from "@react-native-async-storage/async-storage"
+import i18n from "@/i18"
+import { setLang } from "@/store/lang.store"
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
+SplashScreen.preventAutoHideAsync()
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
+  const colorScheme = useColorScheme()
   const [loaded] = useFonts({
     SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
-  });
-  const { t } = useTranslation();
+  })
+  const { t } = useTranslation()
 
   useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
+    async function getLang() {
+      const lang = await AsyncStorage.getItem("language")
+      lang && dispatch(setLang(lang))
     }
-  }, [loaded]);
 
-  if (!loaded) {
-    return null;
-  }
+    getLang()
+
+    if (loaded) {
+      SplashScreen.hideAsync()
+    }
+  }, [loaded])
+
+  if (!loaded) return null
 
   const theme = {
     ...PaperDefaultTheme,
@@ -48,7 +56,7 @@ export default function RootLayout() {
       primary: "orange",
       secondary: "white",
     },
-  };
+  }
 
   return (
     <Provider store={store}>
@@ -75,5 +83,5 @@ export default function RootLayout() {
         </PaperProvider>
       </PersistGate>
     </Provider>
-  );
+  )
 }
